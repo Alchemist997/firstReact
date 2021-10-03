a5757cf8797b99a6e6cc700542f3c0360bc74596
Alchemist997 <brainbrain160@gmail.com>
2021-10-04 02:03:10 +0300

    Перенёс данные в state

--- src/index.js
+++ src/index.js
@@ -3,22 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
-const Dialogsdata = [
-  { id: 997, name: 'Dimon', text: 'Hi bro' },
-  { id: 9972, name: 'Dimson', text: 'What\'s up?' },
-  { id: 9973, name: 'Dimas', text: 'Nice' }
-];
-
-const Posts = [
-  { photoSrc: './img/KateK.jpg', name: 'Катюша', text: 'Как погода? азаза', likes: 997 },
-  { photoSrc: './img/Kate.jpg', name: 'Катенька', text: 'Привет, как дела?)', likes: 12 },
-  { photoSrc: './img/Naste.jpg', name: 'Настя', text: 'Люблю бмв *__*', likes: 85850 }
-];
+import state from './redux/state';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App data={Dialogsdata} posts={Posts} />
+    <App data={state.messagesData.dialogs} posts={state.profileData.posts} />
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/redux/state.js
@@ -0,0 +1,18 @@
+let state = {
+    profileData: {
+        posts: [
+            { photoSrc: './img/KateK.jpg', name: 'Катюша', text: 'Как погода? азаза', likes: 997 },
+            { photoSrc: './img/Kate.jpg', name: 'Катенька', text: 'Привет, как дела?)', likes: 12 },
+            { photoSrc: './img/Naste.jpg', name: 'Настя', text: 'Люблю бмв *__*', likes: 85850 }
+        ]
+    },
+    messagesData: {
+        dialogs: [
+            { id: 997, name: 'Dimon', text: 'Hi bro' },
+            { id: 9972, name: 'Dimson', text: 'What\'s up?' },
+            { id: 9973, name: 'Dimas', text: 'Nice' }
+        ]
+    }
+}
+
+export default state
