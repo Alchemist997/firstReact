ada868e6c7e9e2e5cd887858eac5e41cea28f19e
Alchemist997 <brainbrain160@gmail.com>
2021-10-03 23:23:37 +0300

    Вынес данные на компонент выше

--- src/components/main.js
+++ src/components/main.js
@@ -1,21 +1,30 @@
 import css from './Main.module.css'
 import { Route } from "react-router"
-import messages from "./main/messages"
+import Messages from "./main/messages"
 import profile from "./main/profile"
 import news from "./main/news"
 import music from "./main/music"
 import settings from "./main/settings"
 import Sidebar from "./sidebar"
 
-let Main = () => <main className={css.main}>
-    <Sidebar />
-    <div className={css.article}>
-        <Route path="/messages" component= {messages} />
-        <Route path="/profile" component= {profile} />
-        <Route path="/news" component= {news} />
-        <Route path="/music" component= {music} />
-        <Route path="/settings" component= {settings} />
-    </div>
+let Main = () => {
+
+    const Dialogsdata = [
+        { id: 997, name: 'Dimon', text: 'Hi bro' },
+        { id: 9972, name: 'Dimson', text: 'What\'s up?' },
+        { id: 9973, name: 'Dimas', text: 'Nice' }
+    ];
+
+    return <main className={css.main}>
+        <Sidebar />
+        <div className={css.article}>
+            <Route path="/messages" render={() => <Messages data={Dialogsdata} />} />
+            <Route path="/profile" component={profile} />
+            <Route path="/news" component={news} />
+            <Route path="/music" component={music} />
+            <Route path="/settings" component={settings} />
+        </div>
     </main>
+}
 
 export default Main
--- src/components/main/messages.js
+++ src/components/main/messages.js
@@ -1,12 +1,6 @@
 import css from './Messages.module.css'
 import { NavLink } from "react-router-dom"
 
-const Dialogsdata = [
-    {id: 997, name: 'Dimon', text: 'Hi bro'},
-    {id: 9972, name: 'Dimson', text: 'What\'s up?'},
-    {id: 9973, name: 'Dimas', text: 'Nice'}
-]
-
 const DialogItem = props => {
     return <NavLink to={`/messages/${props.id}`} className={css.dialog}>
         <div>{props.name}</div>
@@ -14,11 +8,14 @@ const DialogItem = props => {
     </NavLink>
 }
 
-const DialogItems = Dialogsdata
-.map( el => <DialogItem name={el.name} text={el.text} id={el.id} />)
+const DialogItems = props => {
+    return props.data.map(el => <DialogItem name={el.name} text={el.text} id={el.id} />)
+}
 
-let messages = () => <div className={css.messages}>
-    {DialogItems}
-</div>
+let Messages = props => {
+    return <div className={css.messages}>
+        <DialogItems data={props.data} />
+    </div>
+}
 
-export default messages
+export default Messages
